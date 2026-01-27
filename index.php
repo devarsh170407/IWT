dbf31b5befa2cc6b7a291ecc3fb45c2d0dad73e8
devarsh170407 <devarshbhatt1747@gmail.com>
2026-01-27 16:14:16 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<table border="solid 2px">
+    <tr>
+        <th>Sr.No</th>
+        <th>Chapter Name</th>
+        <th>Page No</th>
+    </tr>
+    <?php
+    for($i =1;$i<6;$i++){
+    echo '<tr><td>'.$i.'</td><td>Chapter '.$i.'</td><td>'.($i*$i).'</td></tr>';
+    }
+    ?>
+</table>
+
+
+write php scipt which will display 

af89e662d9da5d6fc4ec8cebf0199e3f71fe6ed7
devarsh170407 <devarshbhatt1747@gmail.com>
2026-02-03 15:29:17 +0530

    new

---
+++ calss_work/var_test.php
@@ -0,0 +1,15 @@
+<?php
+    $num = 10;
+    $decnum = 10.5;
+    $is_valid = true;
+    print $num;
+    var_dump($num);
+    echo '<br/>';
+    var_dump($decnum);
+    echo '<br/>';
+    var_dump($is_valid);
+    echo '<br/>';
+    
+    echo $num;
+
+?>

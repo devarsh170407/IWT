4d84d09db78a0105582f14db6c3c9a2928a63bc1
devarsh170407 <devarshbhatt1747@gmail.com>
2026-02-25 13:14:24 +0530

    Create dbconn.php

---
+++ ECOM_APP/dbconn.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$host = "localhost";
+$dbuser = "root";
+$dbpass = "";   // leave empty for XAMPP
+$dbname = "ecom_app_db";
+
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+
+if(!$conn){
+    die("Problem in connection: " . mysqli_connect_error());
+}
+
+echo "Connection done successfully";
+mysqli_close($conn);
+?>

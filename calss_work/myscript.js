ac84bfdbb5d458eed3296b67cd57f82bb397fbe0
devarsh170407 <devarshbhatt1747@gmail.com>
2026-03-18 16:12:12 +0530

    time table

---
+++ calss_work/myscript.js
@@ -0,0 +1,8 @@
+function validate(){
+    let unm = document.getElementById('txtunm')
+    let pwd = document.getElementById('txtpwd')
+    if(unm.value==''||pwd.value=='')
+        {
+    alert('Input field/s are empaty')
+    }
+}
